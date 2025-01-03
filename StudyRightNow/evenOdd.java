961dd6aaaab5875d69a8dbe4dbcdbebaeac0b4b9
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-03 21:55:35 +0630

    EvenOdd

---
+++ StudyRightNow/evenOdd.java
@@ -0,0 +1,19 @@
+package StudyRightNow;
+
+import java.util.*;
+
+public class evenOdd {
+    public static void main(String[] args) {
+        int number;
+        System.out.println("Determining if a number is even or odd");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+}
