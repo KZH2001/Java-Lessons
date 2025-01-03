715ccdf727339933d12c1fdda3ab78e8c0879709
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-03 22:18:35 +0630

    positiveNegative

---
+++ StudyRightNow/positiveNegative.java
@@ -0,0 +1,21 @@
+package StudyRightNow;
+
+import java.util.Scanner;
+
+public class positiveNegative {
+    public static void main(String[] args) {
+        System.out.println("Determing if number is positive or negative");
+        System.out.println("Enter a number");
+ int number;
+ Scanner input = new Scanner(System.in);
+   number = input.nextInt();
+ 
+ if (number > 0){
+    System.out.println("The number is positive");
+ }else if(number == 0){
+    System.out.println("Invalid Number");
+ }else{
+    System.out.println("The number is negative");
+ }       
+    }
+}
