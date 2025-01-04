bfbea6065c660f196dd3255e886f370c3db28256
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-04 11:26:22 +0630

    complete BMI

--- StudyRightNow/bodyMassIndex.java
+++ StudyRightNow/bodyMassIndex.java
@@ -19,7 +19,21 @@ public class bodyMassIndex {
         height = inputHeight * 0.01;
         bmi =  weight / (height * height);
         decimalBMI =  Math.round(bmi * 100) / 100;
+        
         System.out.println("Your BMI is " + decimalBMI);
+
+
+        if (decimalBMI < 18.5) {
+            System.out.println("You are underweight");
+        }else if(decimalBMI >= 18.5 && decimalBMI < 25){
+            System.out.println("You are normal weight");
+        }else if(decimalBMI >= 25 && decimalBMI < 30){
+            System.out.println("You are overweight");
+        }else{
+            System.out.println("You are obese");
+        }
+            
+
 
 }
 }
