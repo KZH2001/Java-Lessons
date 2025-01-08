c2856f9bb8c9c57ddc72cf21e2572a637370e03e
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-08 21:51:19 +0630

    test

--- StudyRightNow/primeApp.java
+++ StudyRightNow/primeApp.java
@@ -1,12 +1,20 @@
-// package StudyRightNow;
+package StudyRightNow;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class primeApp {
+public class primeApp {
 
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         int n = input.nextInt();
-//         primeFunction.isPrime(n);
-// }
-// }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int n = input.nextInt();
+        boolean r = primeFunction.isPrime(n);
+        primeFunction.isPrime(n);
+        if (r == true) {
+            System.out.print(n + " is a Prime number: ");
+        }else{
+            System.out.print(n + " is not a Prime Number:");
+        }
+
+}
+}
