1d36fa1096c3664c3f9cb5f3865c34ee35672d6f
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-09 22:08:38 +0630

    update

--- StudyRightNow/ElpaseDay.java
+++ StudyRightNow/ElpaseDay.java
@@ -8,9 +8,9 @@ public class ElpaseDay {
         String days[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
         int td,ed,fd;
         Scanner input = new Scanner (System.in);
-        System.out.println("Enter today's day: ");
+        System.out.print("Enter today's day: ");
         td = input.nextInt();
-        System.out.println("Enter the number of days elapsed since today: ");
+        System.out.print("Enter the number of days elapsed since today: ");
         ed = input.nextInt();
         fd = (td + ed) % 7;
         System.out.print("Today is " + days[td] + " and the future day is " + days[fd]);
