7351a6c68e737e10163e119b078f8af820a77935
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-12 18:19:58 +0630

    writing object

--- Basic/JdcOnline/Student.java
+++ Basic/JdcOnline/Student.java
@@ -1,5 +1,16 @@
 package JdcOnline;
 
 public class Student {
-    
+
+     
+ 
+ String name;
+ int age;
+
+
+
+void greet(Student otherStudent){
+    System.out.println("Hello " + otherStudent.name + "!");
+    System.out.println("I am " + name + " and I am " + age + " years old.");
+}
 }
