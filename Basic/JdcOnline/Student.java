75d60d6a2c9e4b1bb35e329067ebad7f42fc2f84
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-13 18:21:33 +0630

    update

--- Basic/JdcOnline/Student.java
+++ Basic/JdcOnline/Student.java
@@ -2,13 +2,20 @@ package JdcOnline;
 
 public class Student {
 
-     
- 
  String name;
  int age;
+ static int count;
 
+ public Student() {
+    count++;
+    
+ }
 
-
+ public Student(String name,int age){
+    this.name = name;
+    this.age = age;
+    count++;
+ }
 void greet(Student otherStudent){
     System.out.println("Hello " + otherStudent.name + "!");
     System.out.println("I am " + name + " and I am " + age + " years old.");
