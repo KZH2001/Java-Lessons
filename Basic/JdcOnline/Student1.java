27c4ea2c46bf116e7f1c480c1c674df5939d5de9
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-14 22:10:06 +0630

    test

---
+++ Basic/JdcOnline/Student1.java
@@ -0,0 +1,17 @@
+package JdcOnline;
+
+public class Student1 {
+
+    String name;
+    int age;
+    String email;
+    String address;
+
+    public Student1(String name, int age, String email, String address) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+}
