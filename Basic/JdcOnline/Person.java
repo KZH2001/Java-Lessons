912243ff3362130b2cfb952de4abf60557b0d2bf
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-16 18:22:43 +0630

    update

---
+++ Basic/JdcOnline/Person.java
@@ -0,0 +1,9 @@
+package Basic.JdcOnline;
+
+public class Person{
+    String name;
+    int age;
+    void greet(){
+        System.out.printf("Hello, my name is %s and %e years old %n", name,age);
+    }
+}
