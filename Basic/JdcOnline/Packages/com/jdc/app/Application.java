f22966f4b14b074051b2debf87fddf9e7cac6836
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-17 21:03:38 +0630

    test

--- Basic/JdcOnline/Packages/com/jdc/app/Application.java
+++ Basic/JdcOnline/Packages/com/jdc/app/Application.java
@@ -1,12 +1,25 @@
 package com.jdc.app;
-import com.jdc.app.domain.Student;
+import com.jdc.app.domain.*;
+import static com.jdc.app.utils.MessageUtils.*;
 
-class Application{
+
+public class Application{
+
     public static void main(String[] args) {
+
+        //same package from teacher
         Teacher teacher = new Teacher();
         teacher.greet();
 
+
+
+        //different package from student
         Student student = new Student();
         student.greetStudent();
+
+
+//different package from static method and static message
+        StaticMessage();
+        System.out.println(MESSAGE);
     }
 }
---
+++ Basic/JdcOnline/Packages/com/jdc/app/utils/MessageUtils.java
@@ -0,0 +1,8 @@
+package com.jdc.app.utils;
+public  class MessageUtils {
+    public static String MESSAGE = "This is a static message from Message Utils";
+
+    public static void StaticMessage(){
+        System.out.println("This is a static Method");
+    }
+}
