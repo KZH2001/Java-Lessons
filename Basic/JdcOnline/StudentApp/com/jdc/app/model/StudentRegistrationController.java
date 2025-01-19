4d540df647325d3a6454d1f5e4b95361b56f4a95
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-19 18:27:21 +0630

    update

---
+++ Basic/JdcOnline/StudentApp/com/jdc/app/model/StudentRegistrationController.java
@@ -0,0 +1,21 @@
+package com.jdc.app.model;
+import java.util.Scanner;
+
+public class StudentRegistrationController {
+private Scanner scanner
+
+
+    public void addNewStudent(){
+
+
+    //Show message to enter student name
+        System.out.print("Please Enter Student Name: ");
+
+    //get user input
+        String StudentName = scanner.nextLine();
+
+    //add data to StudentRegistry
+
+
+    }
+}
