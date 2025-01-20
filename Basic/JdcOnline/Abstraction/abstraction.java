37deeb82cf6690546036ef711229b93c00a54172
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-21 01:14:55 +0630

    update

---
+++ Basic/JdcOnline/Abstraction/abstraction.java
@@ -0,0 +1,14 @@
+class Animal{
+    void eat(String food){
+System.out.println("%s is eating %s.".formatted(type,food));
+    }
+}
+
+class Dog extends Animal {}
+
+class Cat extends Animal{}
+
+class Demo{
+
+    void feed(Animal dog )
+}
