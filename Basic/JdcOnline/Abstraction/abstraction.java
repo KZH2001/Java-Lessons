aa6fdeadce8f6c9767a003905d49e206c037dbf2
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-22 21:14:14 +0630

    update

--- Basic/JdcOnline/Abstraction/abstraction.java
+++ Basic/JdcOnline/Abstraction/abstraction.java
@@ -1,14 +1,26 @@
 class Animal{
+
+    private String type;
+
+    Animal(String type){
+        this.type = type;
+    }
     void eat(String food){
 System.out.println("%s is eating %s.".formatted(type,food));
     }
 }
 
-class Dog extends Animal {}
+class Dog extends Animal {
+    public Dog(){
+        super("Dog");
+    }
+}
 
 class Cat extends Animal{}
 
 class Demo{
 
-    void feed(Animal dog )
+    void feed(Animal animal ){
+        animal.eat("food");
+    }
 }
