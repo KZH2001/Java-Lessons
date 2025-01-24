26c922c3d325ccb55d256b72ebf9d4b7298d22db
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-24 20:37:00 +0630

    final

--- Basic/JdcOnline/FinalModifier/Child.java
+++ Basic/JdcOnline/FinalModifier/Child.java
@@ -1,3 +1,17 @@
 public class Child extends Super{
-    
+
+    private String name;
+
+    public Child(String name){
+        super(name);
+        this.name = name.toUpperCase();
+    }
+
+
+    public void greet(){
+        System.out.println("Hello from child! I am %s.". formatted(name));
+        System.out.println("Name from super call is %s." .formatted(super.name));
+
+    }
+
 }
