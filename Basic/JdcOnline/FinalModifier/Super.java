c479c1474bc5175027cc2eabb3e4377d4bfe0655
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-01-26 20:25:18 +0630

    update

--- Basic/JdcOnline/FinalModifier/Super.java
+++ Basic/JdcOnline/FinalModifier/Super.java
@@ -1,14 +1,22 @@
 public class Super {
     public String name;
     public static int count;
+
+
+    //constructor
     public Super(String name){
         this.name = name;
         count++;
     }
+
+
+    //method
     public void greet(){
         System.out.println("Hello! I am %s.". formatted(name));
     }
 
+
+    //static method
     public static void showCount(){
         System.out.println("Cound is %d.".formatted(count));
     }
