1fd3bf8c7f9c97eebaf972c9fa850fe90eec2c99
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-02-04 22:05:23 +0630

    update

--- Basic/JdcOnline/WhileDemo.java
+++ Basic/JdcOnline/WhileDemo.java
@@ -1,20 +1,49 @@
+import java.util.Scanner;
+
 public class WhileDemo {
+
+    public static void main(String[] args) {
+        WhileDemo demo = new WhileDemo();
+        demo.launch();
+    }
+
+
+//    private Scanner scanner = new Scanner(System.in);
     public void launch(){
+        Scanner scanner = new Scanner(System.in);
 
         showMessage();
+        boolean again = true;
+        while(again){
+            int input1,input2;
+//            int input1 = getUserInput("Enter Digit 1: ");
+//            int input2 = getUserInput("Enter Digit 2: ");
 
-        while(){
-            int input1 = getUserInput("Enter Digit 1: ");
-            int input2 = getUserInput("Enter Digit 2: ");
+            System.out.print("Enter Digit 1: ");
+            input1 = scanner.nextInt();
+
+            System.out.print("Enter Digit 2: ");
+            input2 = scanner.nextInt();
+
+
+
             int result = input1 + input2;
-            System.out.printf("%d + %d = %d%n%n", input1,input2,result);
+//            System.out.printf("%d + %d = %d%n%n", input1,input2,result);
+            System.out.println("Result: " + result);
+
+            System.out.print("Do you want to calculate again? (Y/N): ");
+            String input = scanner.next();
+            again = askUserAskAgain(input);
         }
     }
 
 
-    private int getUserInput(String message){
-
-    }
+//    get user information method
+//    private int getUserInput(String message){
+//        System.out.print(message);
+//        var input = scanner.nextLine();
+//        return Integer.parseInt(input);
+//    }
 
     private void showMessage(){
         System.out.println("========================");
@@ -22,4 +51,13 @@ public class WhileDemo {
         System.out.println("========================");
         System.out.println();
         }
-}
+
+
+        private boolean askUserAskAgain(String input) {
+
+//        var input = scanner.nextLine();
+//        return input.equalsIgnoreCase("Y");
+        return "Y".equalsIgnoreCase(input);
+
+        }
+        }
