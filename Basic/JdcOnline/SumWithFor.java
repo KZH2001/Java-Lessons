c3a18e28a5d4b99cb820075e8ddb68a62a02fd69
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-02-05 22:01:56 +0630

    update

---
+++ Basic/JdcOnline/SumWithFor.java
@@ -0,0 +1,13 @@
+public class SumWithFor {
+
+    public static void main(String[] args) {
+      
+      private static void sum(int [] array){
+        int total = 0;
+        for(var i = 0; i < array.length; i++){
+          var element = array[i];
+          total += element;
+      }
+      System.out.println("Total: " + total);
+}       
+}
