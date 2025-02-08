194e5ca3168d2ef7e93e2a161622c464407ae50c
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2025-02-08 21:55:48 +0630

    update

--- Basic/JdcOnline/CheckPrimeNumber.java
+++ Basic/JdcOnline/CheckPrimeNumber.java
@@ -9,38 +9,56 @@ public class CheckPrimeNumber {
     }
 
     //launch method
-    private void launch(){
+    public void launch() {
         welcomeMessaage("Check Prime Number");
-        checkNumber();
+
+        boolean again = true;
+        while (again) {
+            Scanner input = new Scanner(System.in);
+            System.out.println();
+            System.out.print("Enter Your Number : ");
+
+            int num = input.nextInt();
+            if (num <= 1 || num % 2 == 0 && num != 2) {
+                System.out.printf(num + " is not a prime number.%n");
+            } else {
+                System.out.printf(num + " is a prime number.%n");
+            }
+
+            System.out.println();
+            again = checkAgain();
+        }
+        GoodByeMessage("Thank you! See you again!");
     }
 
 
-    //check is number prime or not
-    private void checkNumber(){
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter Your Number : ");
 
-      int num = input.nextInt();
-         if (num <= 1 || num % 2  == 0 && num != 2  ){
-              System.out.printf(num + " is not a prime number.%n");
-          }else {
-             System.out.println( num + " is a prime number.%n");
-         }
-         System.out.println();
-            System.out.println("Do you want to check again?(y/ Others) : ");
-    }
 
     //welcome message
     private void welcomeMessaage(String message){
-        System.out.println("======================");
-        System.out.println(message);
-        System.out.println("======================");
 
+        System.out.println("===================================");
+        System.out.println(message);
+        System.out.println("===================================");
     }
 
+    //GoodBye message
+     private void GoodByeMessage(String message){
+         System.out.println();
+         System.out.println("===================================");
+            System.out.println(message);
+         System.out.println("===================================");
+        }
 
 
 
+    //checkAgain
+    private boolean checkAgain(){
+        System.out.print("Do you want to check again?(y/ Others) : ");
+        Scanner input = new Scanner(System.in);
+        String check = input.next();
+        return "y".equalsIgnoreCase(check);
+        }
 
 
 
